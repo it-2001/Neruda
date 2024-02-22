17276149f64466418e661a95187ba0d44293f05e
it-2001 <it2001@sspu-opava.cz>
2024-02-23 00:15:59 +0100

    testing zig

---
+++ runtime/build.zig
@@ -0,0 +1,28 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const lib = b.addStaticLibrary(.{
+        .name = "lib",
+        .root_source_file = .{ .path = "lib.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "cli",
+        .root_source_file = .{ .path = "cli.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe.linkLibrary(lib);
+
+    b.installArtifact(lib);
+
+    if (b.option(bool, "enable-demo", "install the demo too") orelse false) {
+        b.installArtifact(exe);
+    }
+}
